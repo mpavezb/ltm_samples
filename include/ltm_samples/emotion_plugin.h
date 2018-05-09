ae3552ac41635e58db5b7c6e1828c4263571ea3a
Matías Pavez <matias.pavez.b@gmail.com>
2018-05-09 19:29:09 -0300

    LTM emotion/location plugins based on fake robot services.

--- include/ltm_samples/emotion_plugin.h
+++ include/ltm_samples/emotion_plugin.h
@@ -1,28 +1,66 @@
 #ifndef LTM_SAMPLES_EMOTION_PLUGIN_H_
 #define LTM_SAMPLES_EMOTION_PLUGIN_H_
 
-#include <ltm/plugins_base.h>
 #include <ros/ros.h>
+#include <ltm/plugins_base.h>
+#include <ltm/GetEpisode.h>
 
-namespace ltm_samples {
-    class EmotionPlugin : public ltm::plugin::EmotionBase {
+namespace ltm_samples
+{
+    class EmotionPlugin : public ltm::plugin::EmotionBase
+    {
     public:
         EmotionPlugin() {}
 
         void initialize(const std::string &param_ns) {
-            ROS_INFO_STREAM("LTM Emotion plugin initialized with ns: " << param_ns);
+            log_prefix = "[LTM][Emotion]: ";
+            ROS_DEBUG_STREAM(log_prefix << "Initializing with ns: " << param_ns);
+            ros::NodeHandle priv("~");
+            client = priv.serviceClient<ltm::GetEpisode>("/robot/fake/get_emotion");
+
+            initialized = false;
+            reinitialize();
         }
 
         void register_episode(uint32_t uid) {
-            ROS_INFO_STREAM("LTM Emotion plugin: registering episode " << uid);
+            ROS_DEBUG_STREAM(log_prefix << "registering episode " << uid);
+            registry[uid] = ros::Time::now();
+
+            if (!initialized) reinitialize();
         }
 
         void unregister_episode(uint32_t uid) {
-            ROS_INFO_STREAM("LTM Emotion plugin: unregistering episode " << uid);
+            ROS_DEBUG_STREAM(log_prefix << "unregistering episode " << uid);
+            registry.erase(uid);
         }
 
         void collect(uint32_t uid, ltm::EmotionalRelevance &msg) {
-            ROS_INFO_STREAM("LTM Emotion plugin: collecting episode " << uid);
+            ROS_DEBUG_STREAM(log_prefix << "collecting episode " << uid);
+            if (!initialized && !reinitialize()) {
+                ROS_WARN_STREAM(log_prefix << "Required service is not present (" << client.getService() << ").");
+                return;
+            }
+            ltm::GetEpisode srv;
+            if (client.call(srv)) {
+                msg = srv.response.episode.relevance.emotional;
+            } else {
+                ROS_WARN_STREAM(log_prefix << "service call failed.(" << client.getService() << ").");
+            }
+        }
+
+    private:
+        ros::ServiceClient client;
+        std::map<uint32_t, ros::Time> registry;
+        bool initialized;
+        std::string log_prefix;
+        bool reinitialize() {
+            if (!client.waitForExistence(ros::Duration(2.0))) {
+                ROS_WARN_STREAM(log_prefix << "Couldn't find service " << client.getService() << ".");
+                return false;
+            }
+            ROS_INFO_STREAM(log_prefix << "initialized.");
+            initialized = true;
+            return true;
         }
     };
 
--- include/ltm_samples/location_plugin.h
+++ include/ltm_samples/location_plugin.h
@@ -1,7 +1,9 @@
 #ifndef LTM_SAMPLES_LOCATION_PLUGIN_H_
 #define LTM_SAMPLES_LOCATION_PLUGIN_H_
-#include <ltm/plugins_base.h>
+
 #include <ros/ros.h>
+#include <ltm/plugins_base.h>
+#include <ltm/GetEpisode.h>
 
 namespace ltm_samples
 {
@@ -11,19 +13,54 @@ namespace ltm_samples
         LocationPlugin(){}
 
         void initialize(const std::string& param_ns) {
-            ROS_INFO_STREAM("LTM Location plugin initialized with ns: " << param_ns);
+            log_prefix = "[LTM][Location]: ";
+            ROS_DEBUG_STREAM(log_prefix << "Initializing with ns: " << param_ns);
+            ros::NodeHandle priv("~");
+            client = priv.serviceClient<ltm::GetEpisode>("/robot/fake/get_location");
+
+            initialized = false;
+            reinitialize();
         }
 
         void register_episode(uint32_t uid) {
-            ROS_INFO_STREAM("LTM Location plugin: registering episode " << uid);
+            ROS_DEBUG_STREAM(log_prefix << "registering episode " << uid);
+            registry[uid] = ros::Time::now();
+
+            if (!initialized) reinitialize();
         }
 
         void unregister_episode(uint32_t uid) {
-            ROS_INFO_STREAM("LTM Location plugin: unregistering episode " << uid);
+            ROS_DEBUG_STREAM(log_prefix << "unregistering episode " << uid);
+            registry.erase(uid);
         }
 
         void collect(uint32_t uid, ltm::Where& msg) {
-            ROS_INFO_STREAM("LTM Location plugin: collecting episode " << uid);
+            ROS_DEBUG_STREAM(log_prefix << "collecting episode " << uid);
+            if (!initialized && !reinitialize()) {
+                ROS_WARN_STREAM(log_prefix << "Required service is not present (" << client.getService() << ").");
+                return;
+            }
+            ltm::GetEpisode srv;
+            if (client.call(srv)) {
+                msg = srv.response.episode.where;
+            } else {
+                ROS_WARN_STREAM(log_prefix << "service call failed.(" << client.getService() << ").");
+            }
+        }
+
+    private:
+        ros::ServiceClient client;
+        std::map<uint32_t, ros::Time> registry;
+        bool initialized;
+        std::string log_prefix;
+        bool reinitialize() {
+            if (!client.waitForExistence(ros::Duration(2.0))) {
+                ROS_WARN_STREAM(log_prefix << "Couldn't find service " << client.getService() << ".");
+                return false;
+            }
+            ROS_INFO_STREAM(log_prefix << "initialized.");
+            initialized = true;
+            return true;
         }
     };
 
