a1e92f72ac7caaa4595d375d0ddab8ecccdc5a36
Matías Pavez <matias.pavez.b@gmail.com>
2018-06-07 13:03:57 -0400

    add PeopleEntitySrv ROS srv. Dummy people entity plugin based on image stream plugin

--- include/ltm_samples/plugin/people_entity_plugin.h
+++ include/ltm_samples/plugin/people_entity_plugin.h
@@ -3,74 +3,39 @@
 
 #include <ros/ros.h>
 #include <ltm/plugin/entity_base.h>
-
+#include <ltm/plugin/entity_default.h>
 #include <ltm_samples/PersonEntity.h>
+#include <ltm_samples/PersonEntitySrv.h>
 #include <std_srvs/Empty.h>
-
-#include <warehouse_ros/message_with_metadata.h>
-#include <warehouse_ros_mongo/database_connection.h>
-
-typedef warehouse_ros::MessageCollection<ltm_samples::PersonEntity> PersonEntityCollection;
-typedef boost::shared_ptr<PersonEntityCollection> PersonEntityCollectionPtr;
-
-typedef warehouse_ros::MessageWithMetadata<ltm_samples::PersonEntity> PersonEntityWithMetadata;
-typedef boost::shared_ptr<const PersonEntityWithMetadata> PersonEntityWithMetadataPtr;
 
 namespace ltm_samples
 {
-    class PeopleEntityPlugin : public ltm::plugin::EntityBase
+    class PeopleEntityPlugin :
+            public ltm::plugin::EntityBase,
+            public ltm::plugin::EntityDefault<ltm_samples::PersonEntity, ltm_samples::PersonEntitySrv>
     {
     private:
+        typedef ltm_samples::PersonEntity EntityType;
 
-        // plugin
-        std::string _log_prefix;
-        std::string _type;
-        std::string _collection_name;
-        std::vector<uint32_t> registry;
+        // plugin specific variables
 
         // ROS API
-        ros::ServiceServer _status_service;
-        ros::ServiceServer _drop_db_service;
-        ros::ServiceServer _add_stream_service;
-        ros::ServiceServer _get_stream_service;
-        ros::ServiceServer _delete_stream_service;
-
-        // database
-        DBConnectionPtr _conn;
-        PersonEntityCollectionPtr _coll;
-        std::string _db_name;
-
-        // DB API
-        MetadataPtr make_metadata(const PersonEntity &entity);
-        bool insert(const PersonEntity &entity);
-        bool get(uint32_t uid, PersonEntityWithMetadataPtr &entity_ptr);
-        bool update(uint32_t uid, const PersonEntity &stream);
+        std::string _log_prefix;
 
     public:
         PeopleEntityPlugin(){}
         ~PeopleEntityPlugin();
 
+    private:
+        // DB API
+        MetadataPtr make_metadata(const EntityType &entity);
+
+    public:
         void initialize(const std::string &param_ns, DBConnectionPtr ptr, std::string db_name);
         void register_episode(uint32_t uid);
         void unregister_episode(uint32_t uid);
         void collect(uint32_t uid, ltm::What &msg, ros::Time _start, ros::Time _end);
-        std::string get_type();
-        std::string get_collection_name();
 
-        // - - - - - - - - -  - DB API - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        bool remove(uint32_t uid);
-        int count();
-        bool has(int uid);
-        bool is_reserved(int uid);
-        bool drop_db();
-        void setup_db();
-
-        // ROS API
-        bool status_service(std_srvs::Empty::Request  &req, std_srvs::Empty::Response &res);
-        bool drop_db_service(std_srvs::Empty::Request  &req, std_srvs::Empty::Response &res);
-//        bool add_service(ltm_samples::ImageStreamSrv::Request  &req, ltm_samples::ImageStreamSrv::Response &res);
-//        bool get_service(ltm_samples::ImageStreamSrv::Request  &req, ltm_samples::ImageStreamSrv::Response &res);
-//        bool delete_service(ltm_samples::ImageStreamSrv::Request  &req, ltm_samples::ImageStreamSrv::Response &res);
     };
 
 };
--- src/plugin/people_entity_plugin.cpp
+++ src/plugin/people_entity_plugin.cpp
@@ -3,47 +3,18 @@
 
 namespace ltm_samples
 {
+    // =================================================================================================================
+    // Public API
+    // =================================================================================================================
 
     void PeopleEntityPlugin::initialize(const std::string &param_ns, DBConnectionPtr ptr, std::string db_name) {
         _log_prefix = "[LTM][People Entity]: ";
         ROS_DEBUG_STREAM(_log_prefix << "plugin initialized with ns: " << param_ns);
 
-        // ROS Parameter Server
-        ltm::ParameterServerWrapper psw("~");
-        psw.getParameter(param_ns + "type", _type, "images");
-        psw.getParameter(param_ns + "collection", _collection_name, "image_streams");
 
-        // DB connection
-        _conn = ptr;
-        _db_name = db_name;
-        setup_db();
-
-        // ROS API
-        ros::NodeHandle priv("~");
-        _status_service = priv.advertiseService("entity/" + _type + "/status", &PeopleEntityPlugin::status_service, this);
-        _drop_db_service = priv.advertiseService("entity/" + _type + "/drop_db", &PeopleEntityPlugin::drop_db_service, this);
-//        _add_stream_service = priv.advertiseService("entity/" + _type + "/add", &PeopleEntityPlugin::add_service, this);
-//        _get_stream_service = priv.advertiseService("entity/" + _type + "/get", &PeopleEntityPlugin::get_service, this);
-//        _delete_stream_service = priv.advertiseService("entity/" + _type + "/delete", &PeopleEntityPlugin::delete_service, this);
     }
 
     PeopleEntityPlugin::~PeopleEntityPlugin() {
-
-    }
-
-    MetadataPtr PeopleEntityPlugin::make_metadata(const PersonEntity &entity) {
-
-    }
-
-    bool PeopleEntityPlugin::insert(const PersonEntity &entity) {
-
-    }
-
-    bool PeopleEntityPlugin::get(uint32_t uid, PersonEntityWithMetadataPtr &entity_ptr) {
-
-    }
-
-    bool PeopleEntityPlugin::update(uint32_t uid, const PersonEntity &stream) {
 
     }
 
@@ -59,50 +30,12 @@ namespace ltm_samples
 
     }
 
-    std::string PeopleEntityPlugin::get_type() {
-        return _type;
-    }
+    // =================================================================================================================
+    // Private API
+    // =================================================================================================================
 
-    std::string PeopleEntityPlugin::get_collection_name() {
-        return _collection_name;
-    }
-
-    // - - - - - - - - -  - DB API - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    bool PeopleEntityPlugin::remove(uint32_t uid) {
+    MetadataPtr PeopleEntityPlugin::make_metadata(const PersonEntity &entity) {
 
     }
-
-    int PeopleEntityPlugin::count() {
-
-    }
-
-    bool PeopleEntityPlugin::has(int uid) {
-
-    }
-
-    bool PeopleEntityPlugin::is_reserved(int uid) {
-
-    }
-
-    bool PeopleEntityPlugin::drop_db() {
-
-    }
-
-    void PeopleEntityPlugin::setup_db() {
-
-    }
-
-    // ROS API
-    bool PeopleEntityPlugin::status_service(std_srvs::Empty::Request  &req, std_srvs::Empty::Response &res) {
-
-    }
-
-    bool PeopleEntityPlugin::drop_db_service(std_srvs::Empty::Request  &req, std_srvs::Empty::Response &res) {
-
-    }
-
-//        bool PeopleEntityPlugin::add_service(ltm_samples::ImageStreamSrv::Request  &req, ltm_samples::ImageStreamSrv::Response &res);
-//        bool PeopleEntityPlugin::get_service(ltm_samples::ImageStreamSrv::Request  &req, ltm_samples::ImageStreamSrv::Response &res);
-//        bool PeopleEntityPlugin::delete_service(ltm_samples::ImageStreamSrv::Request  &req, ltm_samples::ImageStreamSrv::Response &res);
 
 }
