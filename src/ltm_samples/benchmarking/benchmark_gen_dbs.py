9f7f7f513c8714743fb0db66717589b359b3551b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-14 01:09:43 +0000

    Adding tarfile member sanitization to extractall()

--- src/ltm_samples/benchmarking/benchmark_gen_dbs.py
+++ src/ltm_samples/benchmarking/benchmark_gen_dbs.py
@@ -143,7 +143,26 @@ class BenchmarkDBGenerator(object):
             return False
 
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall(self.gen_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.gen_path)
         return True
 
     def gzip(self, directory):
